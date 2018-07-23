7222db813d3fb8f755a9ace7bf67ff71d0ff9d46
gmcdonald <gmcdonald@apache.org>
2018-07-23 11:31:23 +1000

    tweaks, so we actually get the remote disk value nad not local

--- modules/jenkins_asf/files/trim-workspace-all-jobs.sh
+++ modules/jenkins_asf/files/trim-workspace-all-jobs.sh
@@ -1,5 +1,22 @@
 #!/usr/local/bin/bash
 
+############################################################################
+# Licensed to the Apache Software Foundation (ASF) under one or more       #
+# contributor license agreements.  See the NOTICE file distributed with    #
+# this work for additional information regarding copyright ownership.      #
+# The ASF licenses this file to you under the Apache License, Version 2.0  #
+# (the "License"); you may not use this file except in compliance with     #
+# the License.  You may obtain a copy of the License at                    #
+#                                                                          #
+#     http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+
 # This script does a find for files older than an optional 
 # arg or default value and deletes files that match. It 
 # searches in directories based on $JENKINS_HOME and below
@@ -7,7 +24,7 @@
 
 RUN_AS_USER=jenkins
 NODES_DIR=/x1/jenkins/jenkins-home/nodes/
-NODES_LIST='/tmp/nodeslist.txt'
+NODES_LIST='/tmp/nodeslist2.txt'
 LOGS_DIR='/x1/jenkins/logs'
 
 # node specific vars
@@ -27,12 +44,14 @@ fi
 # cd $NODES_DIR
 # grep -ir host\> */config.xml | cut -d \> -f2 | cut -d \< -f1 > $NODES_LIST
 
+# TODO Add check to only trim nodes if disk space more than 85% (?) full
+
 if [[ -f "$NODES_LIST" ]]
   then
   while read -r node
   do
     echo "Logging in to node $node"
-ssh -o BatchMode=yes -o ConnectionAttempts=1 -Ti $HOME/.ssh/id_rsa $node <<EOT
+ssh -o BatchMode=yes -o ConnectionAttempts=1 -Ti $HOME/.ssh/id_rsa $node <<'EOT'
 
 declare -a dirs=(
 ".m2/repository"
@@ -45,9 +64,9 @@ declare -a dirs=(
 echo "Disk space before cleanup was:" > /tmp/diskcleanup.txt
 df -h | grep ^/dev/sda1 >> /tmp/diskcleanup.txt
 
-declare SPACE=`df -H | grep x1 | awk '{print $5}' | cut -d'%' -f1`
-echo "Space used = \$SPACE"
-if [ "\$SPACE" -gt 85 ];then
+declare SPACE=`df -H | grep sda1 | awk '{print $5}' | cut -d'%' -f1`
+echo "Space used = $SPACE"
+if [ "$SPACE" -gt 85 ];then
 for dir in "\${dirs[@]}"
  do
   echo "checking for existance of $JENKINS_HOME/\$dir"
