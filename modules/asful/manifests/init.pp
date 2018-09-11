53db2937ee8c3877333beb3e89843b4bc1a276fa
Daniel Gruno <humbedooh@apache.org>
2018-09-11 15:30:43 +0200

    Initial checkout for webstats script
    It's ugly as heck, but it works..

--- modules/asful/manifests/init.pp
+++ modules/asful/manifests/init.pp
@@ -16,4 +16,31 @@ class asful (
       group  => 'elasticsearch',
       content => template('asful/yaml.erb'),
     }
+    
+  file {
+    '/usr/local/etc/webstats.py':
+      ensure => file,
+      mode   => '0755',
+      owner  => 'www-data',
+      group  => 'www-data',
+      source => 'puppet:///modules/asful/webstats.py';
+    '/var/www/snappy/domains.txt':
+      ensure => file,
+      mode   => '0755',
+      owner  => 'www-data',
+      group  => 'www-data',
+      source => 'puppet:///modules/asful/webstats-domains.txt';
+  }
+  
+  cron {
+    'run-webstats':
+      user        => 'www-data',
+      minute      => '0',
+      hour        => '*/4',
+      command     => "cd /usr/local/etc && python webstats.py > /dev/null",
+      environment => [
+        'PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+        'SHELL=/bin/sh'
+      ];
+  }
 }
