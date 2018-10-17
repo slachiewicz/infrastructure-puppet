9495b4677c655eb3a0cf41469b7b267c84ba6973
gmcdonald <gmcdonald@apache.org>
2018-10-17 21:06:53 +1100

    of: quotes already included

--- modules/qmail_asf/manifests/init.pp
+++ modules/qmail_asf/manifests/init.pp
@@ -313,7 +313,7 @@ class qmail_asf (
   exec { 'control-files':
     command => 'svn co https://svn.apache.org/repos/infra/infrastructure/trunk/qmail/control/',
     path    => '/usr/bin/',
-    cwd     => '$qmail_dir',
+    cwd     => $qmail_dir,
     user    => $username,
     group   => $groupname,
     creates => "${control_dir}/me.asf",
