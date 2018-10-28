c3c5bb3cffb93fcd957cf4342a1d6e945a05cbe0
gmcdonald <gmcdonald@apache.org>
2018-10-28 11:06:59 +1100

    commas

--- modules/httpd_asf/manifests/low.pp
+++ modules/httpd_asf/manifests/low.pp
@@ -3,16 +3,16 @@
 # httpd:asf low class
 class httpd_asf::low (
 
-$::apache::mod::event::listenbacklog = '511'
-$::apache::mod::event::maxclients = '50'
-$::apache::mod::event::maxconnectionsperchild = '2000'
-$::apache::mod::event::maxrequestworkers = '20'
-$::apache::mod::event::maxsparethreads = '10'
-$::apache::mod::event::minsparethreads = '5'
-$::apache::mod::event::serverlimit = '5'
-$::apache::mod::event::startservers = '2'
-$::apache::mod::event::threadlimit = '15'
-$::apache::mod::event::threadsperchild = '5'
+$::apache::mod::event::listenbacklog = '511',
+$::apache::mod::event::maxclients = '50',
+$::apache::mod::event::maxconnectionsperchild = '2000',
+$::apache::mod::event::maxrequestworkers = '20',
+$::apache::mod::event::maxsparethreads = '10',
+$::apache::mod::event::minsparethreads = '5',
+$::apache::mod::event::serverlimit = '5',
+$::apache::mod::event::startservers = '2',
+$::apache::mod::event::threadlimit = '15',
+$::apache::mod::event::threadsperchild = '5',
 
 ) {
 
