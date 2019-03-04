620f4fdf1b9acde1f38f74f343e3cc252af12757
Chris Thistlethwaite <christ@apache.org>
2019-03-04 10:37:21 -0500

    adding latest JDK 12 and 13 versions to Windows build nodes

--- environments/windows/modules/jenkins_slave_windows/manifests/init.pp
+++ environments/windows/modules/jenkins_slave_windows/manifests/init.pp
@@ -82,12 +82,12 @@ class jenkins_slave_windows (
     provider => powershell,
   }
   exec { 'create symlink for JDK13':
-    command  => "cmd /c rmdir F:\\jenkins\\tools\\java\\latest13 \"&\" mklink /d F:\\jenkins\\tools\\java\\latest13 F:\\jenkins\\tools\\java\\jdk13-ea+3",# lint:ignore:140chars
+    command  => "cmd /c rmdir F:\\jenkins\\tools\\java\\latest13 \"&\" mklink /d F:\\jenkins\\tools\\java\\latest13 F:\\jenkins\\tools\\java\\jdk13-ea+10",# lint:ignore:140chars
     onlyif   => "if ((Get-Item F:\\tools_zips).LastWriteTime -lt (Get-Date).AddMinutes(-60)) { exit 1;}  else { exit 0; }",
     provider => powershell,
   }
   exec { 'create symlink for JDK12':
-    command  => "cmd /c rmdir F:\\jenkins\\tools\\java\\latest12 \"&\" mklink /d F:\\jenkins\\tools\\java\\latest12 F:\\jenkins\\tools\\java\\jdk12-ea+28",# lint:ignore:140chars
+    command  => "cmd /c rmdir F:\\jenkins\\tools\\java\\latest12 \"&\" mklink /d F:\\jenkins\\tools\\java\\latest12 F:\\jenkins\\tools\\java\\jdk12-ea+33",# lint:ignore:140chars
     onlyif   => "if ((Get-Item F:\\tools_zips).LastWriteTime -lt (Get-Date).AddMinutes(-60)) { exit 1;}  else { exit 0; }",
     provider => powershell,
   }
