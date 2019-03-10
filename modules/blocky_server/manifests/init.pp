e5b0304426ebb6fa84edda2b8097e8485733aaf0
Daniel Gruno <humbedooh@apache.org>
2019-03-10 13:42:56 +0100

    tweak cmd

--- modules/blocky_server/manifests/init.pp
+++ modules/blocky_server/manifests/init.pp
@@ -6,7 +6,7 @@ class blocky_server (
   # Gunicorn for blocky server
   # Run this command unless gunicorn is already running.
   # -w 10 == 10 workers, we can up that if need be.
-  exec { '/usr/bin/gunicorn -w 8 -b 127.0.0.1:8000 -D handler:application':
+  exec { '/usr/bin/gunicorn3 -w 8 -b 127.0.0.1:8000 -D handler:application':
     path   => '/usr/bin:/usr/sbin:/bin',
     user   => 'root',
     group  => 'root',
