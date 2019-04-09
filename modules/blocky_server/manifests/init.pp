1b934cd3a0124fc0a01228bfaefbee0393515da1
Andew Foulks <dfoulks@apache.org>
2019-04-09 11:26:56 -0400

    blocky_server module testing

--- modules/blocky_server/manifests/init.pp
+++ modules/blocky_server/manifests/init.pp
@@ -2,7 +2,22 @@
 
 class blocky_server (
 ){
-  
+  # Checkout latest blocky code from git
+  vcsrepo { '/blocky/blocky2':
+    ensure   => latest,
+    provider => git,
+    source   => 'https://github.com/apache/infrastructure-blocky',
+  }
+  # Copy the httpd config file for the blocky site.
+  # This is a template in case it needs modification
+  # not because it currently does.
+  file { '/etc/apache2/sites-available/25-blocky-ssl.conf':
+    ensure   => file,
+    content  => template('25-blocky-ssl.conf.erb'),
+    user     => 'root',
+    owner    => 'root',
+    mode     => 0644,
+  }  
   # Gunicorn for blocky server
   # Run this command unless gunicorn is already running.
   # -w 10 == 10 workers, we can up that if need be.
