41cc28e33de1d1fb5664469a699b803879c8b63c
Chris Lambertus <cml@apache.org>
2019-07-10 14:11:22 -0700

    bump runtime to catch more systems today

--- modules/asf_checkrootkit/manifests/init.pp
+++ modules/asf_checkrootkit/manifests/init.pp
@@ -3,7 +3,7 @@ class asf_checkrootkit {
   cron { 'checkrootkit':
     command => '/usr/sbin/chkrootkit 2>&1',
     user    => 'root',
-    hour    => 20,
+    hour    => 22,
     minute  => 45,
     require => Package['chkrootkit'],
   }
