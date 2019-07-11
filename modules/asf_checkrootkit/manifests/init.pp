0d4ac740d464a3b2e8123eab0e2a3a00a2657d1b
Chris Lambertus <cml@apache.org>
2019-07-10 18:58:04 -0700

    tone down output, move time
    grep out the "success" messages and some false positives to leave more
    interesting output.

--- modules/asf_checkrootkit/manifests/init.pp
+++ modules/asf_checkrootkit/manifests/init.pp
@@ -1,9 +1,9 @@
 class asf_checkrootkit {
 
   cron { 'checkrootkit':
-    command => '/usr/sbin/chkrootkit 2>&1',
+    command => "/usr/sbin/chkrootkit | /bin/egrep -v 'nothing found|suspicious files|travis.yml|build-id|no suspect|not found|not infected|Windigo|xbuild-framework|nothing detected|nothing deleted|not promisc|dhclient' 2>&1",
     user    => 'root',
-    hour    => 22,
+    hour    => 03,
     minute  => 45,
     require => Package['chkrootkit'],
   }
