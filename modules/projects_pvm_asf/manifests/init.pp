56d26fdf94b06e34f03355771cb4fb8fac4d01c9
Daniel Gruno <humbedooh@apache.org>
2019-08-15 13:12:18 +0200

    Try ::

--- modules/projects_pvm_asf/manifests/init.pp
+++ modules/projects_pvm_asf/manifests/init.pp
@@ -28,14 +28,14 @@ class projects_pvm_asf (
       ensure => present,
       owner  => 'www-data',
       group  => 'www-data',
-      content => hiera('projects_pvm_asf.kibble_token'),
+      content => hiera('projects_pvm_asf::kibble_token'),
       mode   => '0600';
 
     '/usr/local/etc/tokens/jira.txt':
       ensure => present,
       owner  => 'www-data',
       group  => 'www-data',
-      content => hiera('projects_pvm_asf.jira_token'),
+      content => hiera('projects_pvm_asf::jira_token'),
       mode   => '0600';
   }
 
