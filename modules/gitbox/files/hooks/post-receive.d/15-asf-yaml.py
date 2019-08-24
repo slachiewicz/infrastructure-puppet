e3ac32aede0f0d828d7a13c6870fd3ab3b115bc0
Daniel Gruno <humbedooh@apache.org>
2019-08-24 21:32:26 +0200

    Add hook for .asf.yaml parsing
    This is just a test for now, tests the presence and contents of .asf.yaml, borks if it can't parse it. if not present or empty, ignore the repo for now.

---
+++ modules/gitbox/files/hooks/post-receive.d/15-asf-yaml.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import yaml
+import subprocess
+import asfpy.messaging
+import asfgit.cfg as cfg
+
+def get_yaml():
+    committer = cfg.committer
+    blamemail = cfg.remote_user
+    
+    # We just need the first line, as that has the branch affected:
+    line = sys.stdin.readline()
+    if not line:
+        return
+    [oldrev, newrev, refname] = line.split(None, 2)
+    try:
+        ydata = subprocess.check_output(("/usr/bin/git", "show", "%s:.asf.yaml" % refname))
+    except:
+        ydata = ""
+    if not ydata:
+        return
+    try:
+        config = yaml.safe_load(ydata)
+    except yaml.YAMLError as e:
+        aspy.messaging.mail(recipients = [blamemail, 'team@infra.apache.org'], subject = "Failed to parse .asf.yaml in %s!" % cfg.repo_name, message = str(e))
+        return
+    
+    if config:
+        msg = "Found changes pushed by %s:\n\n%s" % (committer, ydata)
+        aspy.messaging.mail(recipient = 'team@infra.apache.org', subject = "Found .asf.yaml in %s (%s)" % (cfg.repo_name, refname), message = msg)
+
+if __name__ == '__main__':        
+    get_yaml()
