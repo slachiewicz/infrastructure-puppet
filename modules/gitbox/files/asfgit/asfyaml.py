91c69768dcff52ceb82208b164926495004b819f
Daniel Gruno <humbedooh@apache.org>
2019-08-27 06:28:40 +0200

    Add staging feature

--- modules/gitbox/files/asfgit/asfyaml.py
+++ modules/gitbox/files/asfgit/asfyaml.py
@@ -1,4 +1,8 @@
-# Skeleton for now, add features later...
+import requests
+import json
+import asfgit.log
+import asfgit.git
+import re
 
 def pelican(cfg, yml):
     pass
@@ -6,3 +10,41 @@ def pelican(cfg, yml):
 def github(cfg, yml):
     pass
 
+def staging(cfg, yml):
+    """ Staging for websites. Sample entry .asf.yaml entry:
+      staging:
+        profile: gnomes
+        # would stage current branch at https://$project-gnomes.staged.apache.org/
+        # omit profile to stage at $project.staged.a.o
+    """
+    # infer project name
+    m = re.match(r"(?:incubator-)?([^-.]+)", cfg.repo_name)
+    pname = m.group(1)
+    
+    # Get branch
+    ref = yml.get('refname', 'master')
+    
+    # Get profile from .asf.yaml, if present
+    profile = yml.get('profile', '')
+    
+    # Try sending staging payload to pubsub
+    try:
+        payload = {
+            'staging': {
+                'project': pname,
+                'source': "https://gitbox.apache.org/repos/asf/%s.git" % cfg.repo_name,
+                'branch': ref,
+                'profile': profile,
+                'pusher': cfg.committer,
+            }
+        }
+        requests.post("http://%s:%s%s" %
+                      (cfg.gitpubsub_host, cfg.gitpubsub_port, cfg.gitpubsub_path),
+                      data = json.dumps(payload))
+        wsname = pname
+        if profile:
+            wsname += '-%s' % profile
+        print("Staging contents at https://%s.staged.apache.org/ ..." % wsname)
+    except Exception as e:
+        print(e)
+        asfgit.log.exception()
