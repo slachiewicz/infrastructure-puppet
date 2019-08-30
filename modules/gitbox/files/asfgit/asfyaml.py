8c294b57eea1caa1cb7db5359576686f3149da1d
Daniel Gruno <humbedooh@apache.org>
2019-08-30 16:47:44 -0500

    Add publish feature, also allow for domain name mapping

--- modules/gitbox/files/asfgit/asfyaml.py
+++ modules/gitbox/files/asfgit/asfyaml.py
@@ -4,6 +4,13 @@ import asfgit.log
 import asfgit.git
 import re
 
+# LDAP to CNAME mappings for some projects
+WSMAP = {
+    'whimsy': 'whimsical',
+    'empire': 'empire-db',
+    'webservices': 'ws'
+}
+
 def pelican(cfg, yml):
     pass
 
@@ -20,6 +27,7 @@ def staging(cfg, yml):
     # infer project name
     m = re.match(r"(?:incubator-)?([^-.]+)", cfg.repo_name)
     pname = m.group(1)
+    pname = WSMAP.get(pname, pname)
     
     # Get branch
     ref = yml.get('refname', 'master').replace('refs/heads/', '')
@@ -50,6 +58,41 @@ def staging(cfg, yml):
         if profile:
             wsname += '-%s' % profile
         print("Staging contents at https://%s.staged.apache.org/ ..." % wsname)
+    except Exception as e:
+        print(e)
+        asfgit.log.exception()
+def publish(cfg, yml):
+    """ Publishing for websites. Sample entry .asf.yaml entry:
+      publish:
+        whoami: asf-site
+        # would publish current branch (if asf-site) at https://$project.apache.org/
+    """
+    # infer project name
+    m = re.match(r"(?:incubator-)?([^-.]+)", cfg.repo_name)
+    pname = m.group(1)
+    pname = WSMAP.get(pname, pname)
+    # Get branch
+    ref = yml.get('refname', 'master').replace('refs/heads/', '')
+    
+    # If whoami specified, ignore this payload if branch does not match
+    whoami = yml.get('whoami')
+    if whoami and whoami != ref:
+        return
+    # Try sending publish payload to pubsub
+    try:
+        payload = {
+            'publish': {
+                'project': pname,
+                'source': "https://gitbox.apache.org/repos/asf/%s.git" % cfg.repo_name,
+                'branch': ref,
+                'pusher': cfg.committer,
+            }
+        }
+        requests.post("http://%s:%s%s" %
+                      (cfg.gitpubsub_host, cfg.gitpubsub_port, cfg.gitpubsub_path),
+                      data = json.dumps(payload))
+        
+        print("Publishing contents at https://%s.apache.org/ ..." % pname)
     except Exception as e:
         print(e)
         asfgit.log.exception()
