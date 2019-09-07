d54acfb0a24e031dffa4adba5f7cf5c85ea2bf75
Chris Lambertus <cml@apache.org>
2019-09-07 10:36:18 -0700

    INFRA-18991
    add exclusion for postfix port
    force root@a.o as the email target

--- modules/asf_checkrootkit/manifests/init.pp
+++ modules/asf_checkrootkit/manifests/init.pp
@@ -1,11 +1,12 @@
 class asf_checkrootkit {
 
   cron { 'checkrootkit':
-    command => "/usr/sbin/chkrootkit | /bin/egrep -v 'nothing found|suspicious files|travis.yml|build-id|no suspect|not found|not infected|Windigo|xbuild-framework|nothing detected|nothing deleted|not promisc|dhclient' 2>&1",
-    user    => 'root',
-    hour    => 03,
-    minute  => 45,
-    require => Package['chkrootkit'],
+    command     => "/usr/sbin/chkrootkit | /bin/egrep -v 'nothing found|suspicious files|travis.yml|build-id|no suspect|not found|not infected|Windigo|xbuild-framework|nothing detected|nothing deleted|not promisc|dhclient|465' 2>&1",
+    user        => 'root',
+    hour        => 03,
+    minute      => 45,
+    environment => "MAILTO=root@apache.org",
+    require     => Package['chkrootkit'],
   }
 
 }
