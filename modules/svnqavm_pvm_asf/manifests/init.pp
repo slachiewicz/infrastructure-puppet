9c62e9edb01ed3660b5229d8016e7264ebcf3dd6
Julian Foad <julianfoad@apache.org>
2019-09-17 18:10:26 +0100

    Comment out all but wayita
    as part of switching from svn-qavm3 to new svn-qavm. (Wayita is not transferred to svn-qavm yet.)

--- modules/svnqavm_pvm_asf/manifests/init.pp
+++ modules/svnqavm_pvm_asf/manifests/init.pp
@@ -40,35 +40,35 @@ class svnqavm_pvm_asf (
       hour    => 14,
       minute  => 23;
 
-    # Run backport merges
-    'backport-cron':
-      command     => 'for i in 1.9.x 1.10.x 1.12.x; do cd && cd src/svn/$i && $SVN up -q --non-interactive && YES=1 MAY_COMMIT=1 ../trunk/tools/dist/backport.pl; done', # lint:ignore:140chars
-      user        => 'svnsvn',
-      hour        => 4,
-      minute      => 0,
-      environment => 'SVN=svn';
-    # Log the revision of backport.pl in use.
-    # (There's no log rotation since this will use about 2KB a year.)
-    'backport-version-log':
-      command     => '(date +\%Y\%m\%d: ; $SVNVERSION src/svn/trunk/tools/dist/backport.pl) >> ~/live-version.log',
-      user        => 'svnsvn',
-      hour        => 4,
-      minute      => 0,
-      environment => 'SVNVERSION=svnversion';
-
-    'Update our Haxx-URL-to-Message-Id map':
-      command     => 'cd ~/src/svn/site && ${SVN} up -q && tools/generate-message-id-map.py',
-      user        => 'svnsvn',
-      hour        => 4,
-      minute      => 0,
-      environment => 'SVN=svn';
-
-    'Update our upcoming changes list':
-      command     => 'chronic ~/src/svn/site/tools/generate-upcoming-changes-log.sh',
-      user        => 'svnsvn',
-      hour        => 4,
-      minute      => 15,
-      environment => 'SVN=svn';
+#    # Run backport merges
+#    'backport-cron':
+#      command     => 'for i in 1.9.x 1.10.x 1.12.x; do cd && cd src/svn/$i && $SVN up -q --non-interactive && YES=1 MAY_COMMIT=1 ../trunk/tools/dist/backport.pl; done', # lint:ignore:140chars
+#      user        => 'svnsvn',
+#      hour        => 4,
+#      minute      => 0,
+#      environment => 'SVN=svn';
+#    # Log the revision of backport.pl in use.
+#    # (There's no log rotation since this will use about 2KB a year.)
+#    'backport-version-log':
+#      command     => '(date +\%Y\%m\%d: ; $SVNVERSION src/svn/trunk/tools/dist/backport.pl) >> ~/live-version.log',
+#      user        => 'svnsvn',
+#      hour        => 4,
+#      minute      => 0,
+#      environment => 'SVNVERSION=svnversion';
+#
+#    'Update our Haxx-URL-to-Message-Id map':
+#      command     => 'cd ~/src/svn/site && ${SVN} up -q && tools/generate-message-id-map.py',
+#      user        => 'svnsvn',
+#      hour        => 4,
+#      minute      => 0,
+#      environment => 'SVN=svn';
+#
+#    'Update our upcoming changes list':
+#      command     => 'chronic ~/src/svn/site/tools/generate-upcoming-changes-log.sh',
+#      user        => 'svnsvn',
+#      hour        => 4,
+#      minute      => 15,
+#      environment => 'SVN=svn';
 
   }
 
