4775e847eb4f5724fa423154cc143800bf06f6ff
Daniel Gruno <humbedooh@apache.org>
2019-09-24 08:25:26 -0500

    Change to check_output, or no stderr will be returned. also check for bytestring

--- modules/gitbox/files/asfgit/hooks/sync.py
+++ modules/gitbox/files/asfgit/hooks/sync.py
@@ -17,10 +17,13 @@ def main():
        for ref in git.stream_refs(sys.stdin):
           if ref.is_rewrite():
              print("Syncing %s (FORCED)..." % ref.name)
-             subprocess.check_call(["git", "push", "-f", ghurl, "%s:%s" % (ref.newsha, ref.name)])
+             subprocess.check_output(["git", "push", "-f", ghurl, "%s:%s" % (ref.newsha, ref.name)])
           else:
              print("Syncing %s..." % ref.name)
-             subprocess.check_call(["git", "push", ghurl, "%s:%s" % (ref.newsha, ref.name)])
+             subprocess.check_output(["git", "push", ghurl, "%s:%s" % (ref.newsha, ref.name)])
     except subprocess.CalledProcessError as err:
-        util.abort("Could not sync with GitHub: %s" % err.output.decode('utf-8'))
+        what = err.output
+        if type(what) is bytes:
+            what = what.decode('utf-8')
+        util.abort("Could not sync with GitHub: %s" % what)
 
